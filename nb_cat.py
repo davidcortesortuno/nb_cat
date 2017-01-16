738704fc62c59ad873f8f565bd3781f574a27250
davidcorteso <dic1e13@soton.ac.uk>
2017-01-16 10:16:16 +0000

    Added formatting for code cells and added colors

--- nb_cat.py
+++ nb_cat.py
@@ -1,14 +1,30 @@
 import nbformat
 
-cell_type = {'code': 'C', 'markdown': 'M'}
+
+# -----------------------------------------------------------------------------
+
+class tint:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    RED = '\033[93m'
+    FAIL = '\033[91m'
+    END = '\033[0m'
+
+# -----------------------------------------------------------------------------
+
+cell_type = {'code': tint.BLUE + '[C]:' + tint.END,
+             'markdown': tint.GREEN + '[M]:' + tint.END}
 
 with open('FeGe_parameters.ipynb') as nb_file:
     # File instance and notebook version:
     nb = nbformat.read(nb_file, 4)
 
-for cell in nb['cells'][:2]:
-    print('{:<5}: {}'.format(cell_type[cell['cell_type']],
-                             cell['source'] ))
+for cell in nb['cells']:
+    print('{:<6} {}'.format(cell_type[cell['cell_type']],
+                            cell['source'].replace('\n',
+                                                   '\n' + ' ' * 7)
+                            ))
     # for source in cell['source'].split('\n'):
     #     print('     {}'.format(source))
 
